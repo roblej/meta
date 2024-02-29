122d91b6d1509e8d8f5c3ea42aa24df7b9b14829
roblej <tein@kakao.com>
2024-02-29 15:41:00 +0900

    add quiz1 and c-programming test

---
+++ c/checkeod.c
@@ -0,0 +1,7 @@
+int check(int x){
+    if(x%2==1){
+        printf("홀수입니다\n");
+    }
+    else
+        printf("짝수입니다\n");
+}
---
+++ c/quiz1.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include "libcheckeod.h"
+
+void main(){
+    int x;
+    printf("Input one number : ");
+    scanf("%d", &x);
+    check(x);
+}
